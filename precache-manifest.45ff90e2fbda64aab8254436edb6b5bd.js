c5d879c4cde40f3517a9f876f4a3a8f113ae691d
wasiquehaider <wasiquehaider02@gmail.com>
2020-03-18 02:13:54 +0500

    Updates

---
+++ precache-manifest.45ff90e2fbda64aab8254436edb6b5bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8471c070a7d3ea6cc6168f139fdef08",
+    "url": "/covid_19_app/index.html"
+  },
+  {
+    "revision": "e23731794fedd3ed1167",
+    "url": "/covid_19_app/static/css/main.cf4b0280.chunk.css"
+  },
+  {
+    "revision": "0b1ed2b7a308c6e83d57",
+    "url": "/covid_19_app/static/js/2.62054fbc.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/covid_19_app/static/js/2.62054fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23731794fedd3ed1167",
+    "url": "/covid_19_app/static/js/main.63d4a3a7.chunk.js"
+  },
+  {
+    "revision": "45c303cea01c84c34d18",
+    "url": "/covid_19_app/static/js/runtime-main.207528b2.js"
+  }
+]);
